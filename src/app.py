7f287392f30762816b115a9f062dabe28052fc8e
Matias Sandacz <msandacz@medallia.com>
2023-08-28 19:21:21 -0300

    Add URL handling

--- src/app.py
+++ src/app.py
@@ -1,31 +1,48 @@
 import argparse
+from distutils.debug import DEBUG
 import requests
 import json
+from urllib.parse import urljoin, urlencode
+from dateutil.parser import parse as parse_iso8601
+import logging
 
-#python app.py --help
-# TODO: Verify date is formatted as expected. Fail early.
+logging.getLogger().setLevel(level="INFO")
+
 parser = argparse.ArgumentParser(description='Download Crypto Data')
 parser.add_argument('-i', '--id', type=str, help='Coin identifier (e.g. bitcoin)', required=True)
 parser.add_argument('-d', '--date', type=str, help='ISO8601 date (e.g. 2017-12-30)', required=True)
 args = parser.parse_args()
 
-"""
+base_url = 'https://api.coingecko.com/api/v3/'
 
-endpoint = /coins/{id}/history?date=dd-mm-yyyy
+def build_coin_history_url(coin_name, query_params):
+    endpoint = f'coins/{coin_name}/history'
+    full_url = urljoin(base_url, endpoint)
+    if query_params:
+        query_string = urlencode(query_params)
+        full_url = f'{full_url}?{query_string}'
+    return full_url
 
-id va en path
-el date es una variable
+coin_name = args.id
+query_parameters = {
+    'date': args.date
+}
 
-An example endpoint would be :
-"""
+url = build_coin_history_url(coin_name, query_parameters)
 
-# TODO como se llama cada parte?
+try:
 
-url = url library?
+    # Perform the HTTP Request
+    logging.info(f"Performing an HTTP request to {url}")
+    r = requests.get(url)
+    logging.info(f"Status code was {r.status_code}")
+    # Raise an error if the response was an HTTP Error
+    
+    r.raise_for_status()
 
-# Make API request
-r = requests.get('https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2017')
+    # Creates the file if it doesnt exist, and save json
+    with open("response.json", "w") as outfile:
+        json.dump(r.json(), outfile)
 
-# Creates the file if it doesnt exist, and save json
-with open("response.json", "w") as outfile:
-    json.dump(r.json(), outfile)
+except Exception as err:
+    logging.error(err)
